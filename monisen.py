7f4182aa62074c7b324b246600746b4a452f41a4
kary <karywong@Karys-MacBook-Air.local>
2020-02-29 15:03:27 +0800

    improved prime function, not loop un-prime numbers before

--- monisen.py
+++ monisen.py
@@ -1,11 +1,28 @@
+import time
 # define if number is prime number
 def prime(n):
+    primeLst = []
+    # 1 is prime number
     if n == 1:
         return True
-    for i in range(2, n):
-        if n % i == 0:
-            return False
-    return True
+    # if larger than 1, check if n can be divided by primes before n
+    for i in range(2,n+1):
+        found = False
+        # build a prime num list
+        for j in primeLst:
+            if i % j == 0:
+                found = True
+                break
+        # if number's met, not in list means it's prime 
+        if i == n:
+            if not found:
+                return True 
+            else:
+                return False
+        # only add when number can not be devide by current prime list
+        else:
+            if not found:
+                primeLst.append(i)
 
 # find the nth monisen number
 # monisen number: if P is prime, M is prime and M = 2^P - 1. Call M monisen
@@ -21,4 +38,10 @@ def monisen(n):
                 if j == n:
                     return yetM
 
-print(monisen(4))
+print(prime(15))
+
+while True:
+    n = int(input('enter a number:'))
+    startTime = time.time()
+    print(monisen(n))
+    print(time.time()-startTime)
